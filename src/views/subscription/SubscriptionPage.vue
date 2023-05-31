1b5d46082186d69431be817d91d9c1013d4fdd0b
lekhrajshubhu <lekhraj@shubhu.com>
2023-05-31 15:50:52 +0545

    package ui update

--- src/views/subscription/SubscriptionPage.vue
+++ src/views/subscription/SubscriptionPage.vue
@@ -50,16 +50,14 @@
                         <v-row v-if="subscriptions && subscriptions.length">
                             <v-col cols="6" v-for="(subscription, index) in subscriptions" :key="index">
                                 <div class="package-container mt-4">
-                                    <div>
-                                        <span class="price">{{ subscription.price }}</span><sup>$</sup>
+                                    <div class="mb-2">
+                                        <span class="price">{{ formatAmount(subscription.price) }}</span>
                                     </div>
-                                    <p class="mb-0" v-if="subscription.trial_days">
-                                        {{ formatTrialDays(subscription.trial_days) }}</p>
-                                    <!-- <p class="mb-0" v-if="subscription.trial_days">{{formatTrialDays(32)}}</p> -->
-                                    <div class="pt-6 pb-6">
-                                        <p class="f9-bold ma-0 text-capitalize">{{ subscription.name }}</p>
-                                        <p class="f9 ma-0">{{ subscription.type }}</p>
-                                    </div>
+                                    <p class="mb-2 text-capitalize">{{ subscription.name }}</p>
+                                    <h2 class="mb-2" v-if="subscription.trial_days">
+                                        {{ formatTrialDays(subscription.trial_days) }} FREE
+                                    </h2>
+                                   
                                     <v-btn depressed rounded color="white" large block
                                         :to="'/subscription/' + subscription.id">Upgrade</v-btn>
                                 </div>
@@ -274,7 +272,7 @@ export default {
 }
 
 .price {
-    font-size: 1.2rem;
+    font-size: 1.8rem;
     font-weight: 600;
     color: #000000;
 }
