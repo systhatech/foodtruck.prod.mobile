ccc71fc3c59f1c020ea8034318efa7c326286f53
unknown <lekhraj@shubhu.com>
2023-06-21 22:39:27 +0545

    test

--- src/core/services/config_local.js
+++ src/core/services/config_local.js
@@ -1,4 +1,5 @@
 const base_url = "http://127.0.0.1:8000/api/v2"
+const url_base = "http://127.0.0.1:8000"
 const support_url = "https://support.shubhu.com";
 const base_socket_url = "http://localhost:6061"
 const base_support_project_url = "https://yummtrux.systha.com";
@@ -6,4 +7,4 @@ const support_client_username = "truck_quick";
 const key = 'quick_truck';
 const ssn_encryption_key = 'quick_truck';
 const api_key = btoa(btoa("quick_truck"));
-export { base_url, api_key, ssn_encryption_key, base_socket_url, support_url, base_support_project_url, support_client_username, key }
+export { base_url, url_base, api_key, ssn_encryption_key, base_socket_url, support_url, base_support_project_url, support_client_username, key }
